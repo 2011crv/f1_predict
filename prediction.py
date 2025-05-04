41ddcbbeeade411549986ccae91a3fe3d88a9497
liar13cajun <91923298+liar13cajun@users.noreply.github.com>
2025-05-04 17:37:33 +0930

    basic graph

---
+++ prediction.py
@@ -0,0 +1,8 @@
+import fastf1
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.metrics import mean_absolute_error
+
+
